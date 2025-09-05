579e934582c6cb60a30973570a8a98f4f1f65122
Ahmedabnaser <ahmeducfjf@gmail.com>
2025-09-05 04:07:38 +0300

    Adding Serilog for logging Udpated .gitignore to exclude log files Updated DiContainer to register Serilog services Updated ServiceCollectionExtentions to add Serilog configuration Updated Program.cs to use Serilog for logging Updated launchSettings.json for development environment settings

--- Web/SupermarketPOS.Web.Api/Program.cs
+++ Web/SupermarketPOS.Web.Api/Program.cs
@@ -2,36 +2,47 @@
 using Scalar.AspNetCore;
 using SupermarketPOS.Web.Api;
 using SupermarketPOS.Web.Api.Extentions;
-
+using Serilog;
 namespace OnionArch
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.AddServices();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            Log.Information("Starting SupermarketPOS Web API");
+            try
             {
-                app.MapOpenApi();
-                app.MapScalarApiReference();
+                var builder = WebApplication.CreateBuilder(args);
+               
+                // Add services to the container.
+
+                builder.AddServices();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                app.ConfigScalar();
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.UseSerilogRequestLogging();
+                app.MapControllers();
+
+                app.Run();
+
             }
+            catch (Exception ex) {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                Log.Information("Shutting down SupermarketPOS Web API");
+                Log.CloseAndFlush();
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            }
         }
     }
 }
