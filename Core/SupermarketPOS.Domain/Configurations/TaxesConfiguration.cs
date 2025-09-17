fe3c44d52b5fe2e21809ad0d130489ca5a3d91ac
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2025-09-17 19:26:55 +0300

    Feat:Adding Taxes Configuration

---
+++ Core/SupermarketPOS.Domain/Configurations/TaxesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketPOS.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketPOS.Domain.Configurations
+{
+    public class TaxesConfiguration : IEntityTypeConfiguration<Tax>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Tax> builder)
+        {
+            builder.HasMany(t => t.Sales)                // Tax → many Sales
+                .WithOne(s => s.Tax)                  // Sale → one Tax
+                .HasForeignKey(s => s.TaxId)          // FK in Sale
+                .OnDelete(DeleteBehavior.Restrict);   // avoid deleting sales history
+        }
+    }
+}
