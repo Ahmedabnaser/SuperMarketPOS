533accad5885fc8e4efacbae54c56b8d7aee3cb3
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2025-09-17 19:25:59 +0300

    Feat:Adding Sales Configuration

---
+++ Core/SupermarketPOS.Domain/Configurations/SalesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupermarketPOS.Domain.Entites;
+
+
+namespace SupermarketPOS.Domain.Configurations
+{
+    public class SalesConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasMany(s => s.SaleItems)
+                .WithOne(s => s.Sale)
+                .HasForeignKey(s => s.SaleId);
+
+            builder.HasMany(s => s.Payments)
+                .WithOne(p => p.Sale)
+                .HasForeignKey(s => s.SaleId);
+
+        }
+
+    }
+}
