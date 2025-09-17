ecb0cbf4e14ce471c9b3915cdc4444a7ccdd19cb
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2025-09-17 19:24:51 +0300

    Feat:Adding Payment Configuration

---
+++ Core/SupermarketPOS.Domain/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupermarketPOS.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketPOS.Domain.Configurations
+{
+    public class PaymentConfiguration:IEntityTypeConfiguration<Payment>
+    {
+        public void  Configure(EntityTypeBuilder<Payment> builder) {
+
+
+        }
+    }
+}
