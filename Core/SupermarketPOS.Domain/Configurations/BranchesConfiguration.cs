883a1a0d5c8832278e8e5ff43e0f45ea4ff5e585
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2025-09-17 19:23:43 +0300

    Feat:Adding Branch Configuration

---
+++ Core/SupermarketPOS.Domain/Configurations/BranchesConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupermarketPOS.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketPOS.Domain.Configurations
+{
+    public class BranchesConfiguration : IEntityTypeConfiguration<Branch>
+    {
+        public void Configure(EntityTypeBuilder<Branch> builder)
+        {
+            // one to many relationship with branch -> sales
+            builder.HasMany(b => b.Sales)
+                .WithOne(s => s.Branch)
+                .HasForeignKey(b => b.BranchId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // one to many relationship with branch -> users
+            builder.HasMany(b => b.Users)
+                .WithOne(u => u.Branches)
+                .HasForeignKey(b => b.BranchId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(b => b.shifts)
+                 .WithOne(u => u.Branch)
+                 .HasForeignKey(b => b.BranchId)
+                 .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
